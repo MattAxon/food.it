491668b331fb549f45ac5d439c77c5b7f1389fe0
Matt Axon <mattaxon2@gmail.com>
2021-07-29 18:27:57 -0400

    '7/29'

---
+++ venv/app.py
@@ -0,0 +1,111 @@
+from enum import unique
+from re import template
+from flask import Flask
+from flask import url_for
+from flask import render_template
+from flask import Response, request, jsonify
+from flask import redirect
+import requests
+import json
+import os
+#from flask_sqlalchemy import SQLAlchemy
+#from sqlalchemy.sql import text
+
+
+app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/realestatelogin.user'
+#db = SQLAlchemy(app)
+'''
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email, passw):
+        self.username = username
+        self.email = email
+        self.password = passw
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+'''
+
+#admin = User('admin', 'admin@example.com', "examplePassword")
+
+#db.session.add(admin)
+
+rawData = open('raw_data.json')
+
+finalData = open('static/final_data.txt', 'w',)
+
+data = json.load(rawData)
+finalData.write("\n")
+for i in data["meals"]:
+    try: 
+        finalData.write("'" + i["strMeal"]+ "'" + ","+"\n")
+    except Exception as e:
+        continue
+
+
+rawData.close()
+finalData.close()
+
+@app.route("/")
+def homePage():
+    return render_template('index.html')
+
+
+@app.route('/loginPage')
+def gotoLoginPage():
+    return render_template('loginPage.html')
+
+@app.route('/register')
+def gotoRegisterPage():
+    return render_template('register.html')
+
+@app.route('/registerData', methods=['POST'])
+def RegisterData():
+    newData = request.get_json()
+    username = newData['username']
+    password = newData['password']
+    email = newData['email']
+    print(email)
+    print(username)
+    print(password)
+    return 'hi'
+
+@app.route('/login', methods=["POST"])
+def authenticateLogin():
+    newData = request.get_json()
+    username = newData['username']
+    password = newData['password']
+    print(username)
+    print(password)
+    return 'hi'
+
+
+@app.route('/search', methods=['POST', 'GET'])
+def searchFood(term = None):
+    searchtTerm = request.args.get('term')
+    print(searchtTerm)
+    url = 'http://www.themealdb.com/api/json/v1/1/search.php?s=' + searchtTerm
+    foodData = requests.get(url).json()
+    print(foodData)
+    if(foodData['meals'] == None):
+        return 'error bad user input'
+    return foodData
+
+@app.route('/getTags', methods= ['GET'])
+def getSearchTags():
+    return 'hi'
+
+
+
+
+
+
+
+    
+if __name__ == '__main__':
+    app.run(debug=True)
