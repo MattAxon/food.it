b65ce7c3d11c1895b31e5ad4fafa698c6a9b1301
MattAxon <mattaxon2@gmail.com>
2022-01-02 10:26:15 -0500

    archieving

---
+++ venv/foodProject/static/main.js
@@ -0,0 +1,376 @@
+availableTags = [
+'Apple Frangipan Tart',
+'Apple & Blackberry Crumble',
+'Apam balik',
+'Ayam Percik',
+'Bakewell tart',
+'Bread and Butter Pudding',
+'Beef Wellington',
+'Baingan Bharta',
+'Beef Brisket Pot Roast',
+'Beef Sunday Roast',
+'Braised Beef Chilli',
+'Beef stroganoff',
+'Broccoli & Stilton soup',
+'Bean & Sausage Hotpot',
+'Banana Pancakes',
+'Beef Dumpling Stew',
+'Beef and Mustard Pie',
+'Beef and Oyster pie',
+'Blackberry Fool',
+'Battenberg Cake',
+'Beef Bourguignon',
+'Brie wrapped in prosciutto & brioche',
+'Boulangère Potatoes',
+'BeaverTails',
+'Brown Stew Chicken',
+'Beef Lo Mein',
+'Baked salmon with fennel & tomatoes',
+'Budino Di Ricotta',
+'Breakfast Potatoes',
+'Bitterballen (Dutch meatballs)',
+'BBQ Pork Sloppy Joes',
+'Beef Banh Mi Bowls with Sriracha Mayo, Carrot & Pickled Cucumber',
+'Big Mac',
+'Bigos (Hunters Stew)',
+'Boxty Breakfast',
+'Beef Rendang',
+'Chicken Enchilada Casserole',
+'Cream Cheese Tart',
+'Christmas Pudding Flapjack',
+'Chicken Handi',
+'Chicken Alfredo Primavera',
+'Chicken Fajita Mac and Cheese',
+'Cajun spiced fish tacos',
+'Crock Pot Chicken Baked Tacos',
+'Chicken Karaage',
+'Coq au vin',
+'Chilli prawn linguine',
+'Clam chowder',
+'Creamy Tomato Soup',
+'Chicken & mushroom Hotpot',
+'Chicken Couscous',
+'Chocolate Avocado Mousse',
+'Choc Chip Pecan Pie',
+'Chocolate Raspberry Brownies',
+'Chickpea Fajitas',
+'Chicken Ham and Leek Pie',
+'Chicken Parmentier',
+'Carrot Cake',
+'Chelsea Buns',
+'Chocolate Souffle',
+'Chinon Apple Tarts',
+'Chicken Marengo',
+'Canadian Butter Tarts',
+'Chicken Basquaise',
+'Callaloo Jamaican Style',
+'Chicken Congee',
+'Chocolate Caramel Crispy',
+'Chakchouka ',
+'Cashew Ghoriba Biscuits',
+'Corba',
+'Christmas Pudding Trifle',
+'Classic Christmas pudding',
+'Christmas cake',
+'Corned Beef and Cabbage',
+'Crispy Sausages and Greens',
+'Chicken Quinoa Greek Salad',
+'Chick-Fil-A Sandwich',
+'Coddled pork with cider',
+'Dal fry',
+'Dundee cake',
+'Duck Confit',
+'Eton Mess',
+'Eccles Cakes',
+'English Breakfast',
+'Escovitch Fish',
+'Egg Drop Soup',
+'Egyptian Fatteh',
+'Fish pie',
+'French Lentils With Garlic and Thyme',
+'Fettucine alfredo',
+'Full English Breakfast',
+'French Onion Soup',
+'Flamiche',
+'French Omelette',
+'Fish Stew with Rouille',
+'Fennel Dauphinoise',
+'Fruit and Cream Cheese Breakfast Pastries',
+'French Onion Chicken with Roasted Carrots & Mashed Potatoes',
+'Ful Medames',
+'Feteer Meshaltet',
+'Fish fofos',
+'Garides Saganaki',
+'Grilled Mac and Cheese Sandwich',
+'General Tso\'s Chicken',
+'Gigantes Plaki',
+'Gołąbki (cabbage roll)',
+'Grilled Portuguese sardines',
+'Honey Teriyaki Salmon',
+'Hot Chocolate Fudge',
+'Hot and Sour Soup',
+'Honey Balsamic Chicken with Crispy Broccoli & Potatoes',
+'Honey Yogurt Cheesecake',
+'Ham hock colcannon',
+'Irish stew',
+'Jam Roly-Poly',
+'Jerk chicken with rice & peas',
+'Jamaican Beef Patties',
+'Japanese gohan rice',
+'Japanese Katsudon',
+'Kapsalon',
+'Kentucky Fried Chicken',
+'Katsu Chicken curry',
+'Key Lime Pie',
+'Kidney Bean Curry',
+'Kedgeree',
+'Kung Pao Chicken',
+'Kung Po Prawns',
+'Kafteji',
+'Keleya Zaara',
+'Kumpir',
+'Krispy Kreme Donut',
+'Lamb tomato and sweet spices',
+'Lamb Biryani',
+'Lamb Rogan josh',
+'Laksa King Prawn Noodles',
+'Lamb Tagine',
+'Lasagne',
+'Lamb and Potato pie',
+'Lancashire hotpot',
+'Leblebi Soup',
+'Lasagna Sandwiches',
+'Lamb and Lemon Souvlaki',
+'Lamb Tzatziki Burgers',
+'Mediterranean Pasta Salad',
+'Massaman Beef curry',
+'Mushroom & Chestnut Rotolo',
+'Matar Paneer',
+'Minced Beef Pie',
+'McSinghs Scotch pie',
+'Madeira Cake',
+'Montreal Smoked Meat',
+'Ma Po Tofu',
+'Mbuzi Choma (Roasted Goat)',
+'Mince Pies',
+'Moussaka',
+'Mulukhiyah',
+'Mustard champ',
+'Moroccan Carrot Soup',
+'Mee goreng mamak',
+'Nutty Chicken Curry',
+'Nasi lemak',
+'Osso Buco alla Milanese',
+'Oxtail with broad beans',
+'Pad See Ew',
+'Potato Gratin with Chicken',
+'Poutine',
+'Pilchard puttanesca',
+'Pork Cassoulet',
+'Pancakes',
+'Pumpkin Pie',
+'Peanut Butter Cheesecake',
+'Peach & Blueberry Grunt',
+'Parkin Cake',
+'Pear Tarte Tatin',
+'Provençal Omelette Cake',
+'Prawn & Fennel Bisque',
+'Pate Chinois',
+'Pouding chomeur',
+'Peanut Butter Cookies',
+'Pizza Express Margherita',
+'Paszteciki (Polish Pasties)',
+'Pierogi (Polish Dumplings)',
+'Polskie Naleśniki (Polish Pancakes)',
+'Piri-piri chicken and slaw',
+'Portuguese prego with green piri-piri',
+'Portuguese barbecued pork (Febras assadas)',
+'Portuguese fish stew (Caldeirada de peixe)',
+'Portuguese custard tarts',
+'Rigatoni with fennel sausage sauce',
+'Rocky Road Fudge',
+'Recheado Masala Fish',
+'Ribollita',
+'Roasted Eggplant With Tahini, Pine Nuts, and Lentils',
+'Rock Cakes',
+'Ratatouille',
+'Rappie Pie',
+'Red Peas Soup',
+'Roast fennel and aubergine paella',
+'Rosół (Polish Chicken Soup)',
+'Rogaliki (Polish Croissant Cookies)',
+'Roti john',
+'Spaghetti Bolognese',
+'Spicy Arrabiata Penne',
+'Smoky Lentil Chili with Squash',
+'Sticky Toffee Pudding Ultimate',
+'Spicy North African Potato Salad',
+'Stovetop Eggplant With Harissa, Chickpeas, and Cumin Yogurt',
+'Salmon Prawn Risotto',
+'Salted Caramel Cheescake',
+'Seafood fideuà',
+'Spinach & Ricotta Cannelloni',
+'Squash linguine',
+'Spanish Tortilla',
+'Steak and Kidney Pie',
+'Sticky Toffee Pudding',
+'Spotted Dick',
+'Summer Pudding',
+'Summer Pistou',
+'Split Pea Soup',
+'Sugar Pie',
+'Saltfish and Ackee',
+'Sweet and Sour Pork',
+'Szechuan Beef',
+'Shrimp Chow Fun',
+'Salmon Avocado Salad',
+'Salmon Eggs Eggs Benedict',
+'Shakshuka',
+'Smoked Haddock Kedgeree',
+'Stamppot',
+'Snert (Dutch Split Pea Soup)',
+'Spaghetti alla Carbonara',
+'Soy-Glazed Meatloaves with Wasabi Mashed Potatoes & Roasted Carrots',
+'Skillet Apple Pork Chops with Roasted Sweet Potatoes & Zucchini',
+'Strawberry Rhubarb Pie',
+'Stuffed Lamb Tomatoes',
+'Sledz w Oleju (Polish Herrings)',
+'Shawarma',
+'Spring onion and prawn empanadas',
+'Seri muka kuih',
+'Teriyaki Chicken Casserole',
+'Tandoori chicken',
+'Thai Green Curry',
+'Toad In The Hole',
+'Turkey Meatloaf',
+'Tuna Nicoise',
+'Tahini Lentils',
+'Three Fish Pie',
+'Treacle Tart',
+'Tarte Tatin',
+'Three-cheese souffles',
+'Tourtiere',
+'Timbits',
+'Tunisian Orange Cake',
+'Tunisian Lamb Soup',
+'Tuna and Egg Briks',
+'Tamiya',
+'Vegan Lasagna',
+'Vegan Chocolate Cake',
+'Vietnamese Grilled Pork (bun-thit-nuong)',
+'Venetian Duck Ragu',
+'Vegetarian Casserole',
+'Vegetarian Chilli',
+'Vegetable Shepherd’s Pie',
+'White chocolate creme brulee',
+'Wontons',
+'Yaki Udon'
+];
+
+
+function getTags(){
+  jQuery.get("final_data.txt", function(textString){
+    availableTags = textString.split(",\n");
+    console.log(availableTags);
+  });
+}
+
+function searchFood(term){
+  var newUrl = 'search?term=' + term;
+  $.ajax({
+      type: 'GET',
+      url: newUrl,
+      success: function(result){
+          if(result == "error bad user input"){
+            searchbarLabel = document.getElementById('searchbarLabel');
+            searchbarLabel.setAttribute('style', 'visibility: visible;');
+          }else{
+            console.log(result.meals[0]);
+            displayFood(result.meals[0]);
+            let card = document.getElementById('resultCard');
+            card.setAttribute('style', 'visibility: visible;');
+          }
+      },
+      error: function(error){
+        console.log('error');
+          console.log(error);
+      }
+  });
+}
+
+function displayFood(result){
+  document.getElementById('foodNameDisplay').innerText = result.strMeal;
+  document.getElementById("foodArea").innerText = result.strArea;
+  document.getElementById('foodCategory').innerText = result.strCategory;
+  let imgURL = result.strMealThumb;
+  let ingridientList = [];
+  let messurementList = [];
+  populateFoodLists(result, messurementList, ingridientList);
+  console.log(ingridientList, messurementList);
+   document.getElementById('cardImg').setAttribute('src', imgURL);
+   populateTable(messurementList, ingridientList);
+   document.getElementById('directionText').innerText = result.strInstructions;
+}
+
+function searchCloseClicked(){
+  let card = document.getElementById('resultCard');
+  card.setAttribute('style', 'visibility: hidden;');
+}
+
+function populateFoodLists(result, measureList, ingridientList){
+  for(let i = 1; i <= 20; i++){
+    let measureStr = "strMeasure" + i;
+    let ingridientStr = "strIngredient" + i;
+    if(result[ingridientStr] == ""){
+      break;
+    }else{
+      ingridientList.push(result[ingridientStr]);
+      measureList.push(result[measureStr]);
+    }
+  }
+}
+
+function populateTable(measureList, ingridientList){
+  let tableBody = document.getElementById('tableBody');
+  let newTableBody = document.createElement('tbody');
+  newTableBody.setAttribute('id', 'tableBody');
+  for(let i = 0; i < measureList.length; i++){
+    let newTR = document.createElement('tr');
+    let ingridientCell = newTR.insertCell()
+    let measurementCell = newTR.insertCell();
+    ingridientCell.innerText = ingridientList[i];
+    measurementCell.innerText = measureList[i];
+    let trIDString = "row" + i;
+    let ingridientIDString = "ingridient" + i;
+    let measurementIDString = "measurement" + i;
+    newTR.setAttribute('id', trIDString);
+    ingridientCell.setAttribute('id', ingridientIDString);
+    measurementCell.setAttribute('id', measurementIDString);
+    newTR.setAttribute('class', 'Row');
+    ingridientCell.setAttribute('class', 'ingridientCell');
+    measurementCell.setAttribute('class', 'measurementCell');
+    newTableBody.appendChild(newTR);
+  }
+  tableBody.parentElement.replaceChild(newTableBody, tableBody);
+}
+
+$(document).ready(function(){
+  //getTags();
+  searchBar = $("#search-bar");
+  searchBar.autocomplete({
+    source: availableTags
+  });
+  $('#searchClose').click(function(){
+    searchCloseClicked();
+  });
+
+  searchBar.on('keypress', function(e){
+    if(e.which == 13){
+        searchBarVal = searchBar.val();
+        searchbarLabel = document.getElementById('searchbarLabel');
+        searchbarLabel.setAttribute('style', 'visibility: hidden;');
+        searchFood(searchBarVal);
+    }
+  });
+});
+
